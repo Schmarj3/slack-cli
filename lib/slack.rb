4f155848f5cc71b1e57ee61543fbbe233c220acd
Marj E <marj@marjorieetienne.com>
2020-10-07 16:52:15 -0700

    added new prompt options for wave 2

--- lib/slack.rb
+++ lib/slack.rb
@@ -15,7 +15,10 @@ def main
     puts "Please select an option:"
     puts "A. List Users"
     puts "B. List Channels"
-    puts "C. Quit Program"
+    puts "C. Select User"
+    puts "D. Select Channel"
+    puts "E. Details"
+    puts "F. Quit Program"
     user_input = gets.chomp.upcase
 
     case user_input
@@ -23,7 +26,15 @@ def main
       p workspace.users
     when "B", "LIST CHANNELS"
       p workspace.channels
-    when "C", "QUIT"
+    when "C", "SELECT USER"
+      p workspace.selected
+      p "print out selected user as the current recipient"
+    when "D", "SELECT CHANNEL"
+      p workspace.selected
+      p "print out selected channel as the current recipient"
+    when "E", "DETAILS"
+      p "show details on the current recipient"
+    when "F", "QUIT PROGRAM"
       exit_program = true
     else
       puts "Incorrect value, please try again"
