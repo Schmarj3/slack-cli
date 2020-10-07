70de03d7a46ab59a0f053b6d29b1a52fd3389f18
Marj E <marj@marjorieetienne.com>
2020-10-06 23:08:35 -0700

    updated workspace.rb with template code, removed comments from slack.rb

--- lib/slack.rb
+++ lib/slack.rb
@@ -3,7 +3,7 @@ require 'dotenv'
 require 'httparty'
 
 require_relative 'workspace'
-Dotenv.load
+
 
 def main
   puts "Welcome to the Ada Slack CLI!"
@@ -15,45 +15,3 @@ def main
 end
 
 main if __FILE__ == $PROGRAM_NAME
-
-
-
-query_parameters = {
-    token: ENV["SLACK_TOKEN"]
-}
-
-
-url = 'https://slack.com/api/conversations.list'
-response = HTTParty.get(url, query: query_parameters)
-
-# pp response
-#
-#
-# workspace : {
-#     "ok"=> true,
-#     "channel": [
-#         {
-#             general-channel
-#         },
-#         {
-#             random-channel
-#         },
-#         {
-#             ada-slack-cli
-#         }
-#     ]
-# }
-#
-# workspace : {
-#     "ok"=> true,
-#     "channel": [{channel at index 0}, {channel at index 1}, {channel at index 2}]
-#     "channel": ["fire", "air", "water"]
-#     "channel": [0, 1, 2]
-# }
-# puts response["channels"][1]["creater"]
-#
-# response["channels"][i]
-
-# response["channels"].each do |i|
-#   puts i["purpose"]["last_set"]
-# end
--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,3 +1,6 @@
+Dotenv.load
+
+
 class Workspace
   attr_reader :channels, :users
 
@@ -6,5 +9,65 @@ class Workspace
     @users = []
   end
 
+  # def add_channel (channel)
+  # @channels << channel
+  # end
 
+  def select_channel
+    #
+  end
+
+  def select_user
+    #
+  end
+
+  def show_details
+    #
+  end
+
+  def send_message
+    #
+  end
 end
+
+
+
+# query_parameters = {
+#     token: ENV["SLACK_TOKEN"]
+# }
+
+
+# url = 'https://slack.com/api/conversations.list'
+# response = HTTParty.get(url, query: query_parameters)
+
+# pp response
+#
+#
+# workspace : {
+#     "ok"=> true,
+#     "channel": [
+#         {
+#             general-channel
+#         },
+#         {
+#             random-channel
+#         },
+#         {
+#             ada-slack-cli
+#         }
+#     ]
+# }
+#
+# workspace : {
+#     "ok"=> true,
+#     "channel": [{channel at index 0}, {channel at index 1}, {channel at index 2}]
+#     "channel": ["fire", "air", "water"]
+#     "channel": [0, 1, 2]
+# }
+# puts response["channels"][1]["creater"]
+#
+# response["channels"][i]
+
+# response["channels"].each do |i|
+#   puts i["purpose"]["last_set"]
+# end
